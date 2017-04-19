77f063dc3b389eadec1e51392347a58cd1ac975b
ZestyRock <yanguo1208@gmail.com>
2017-04-19 14:09:33 -0700

    Render features for adding/submitting forms

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {TodoForm, TodoList} from './components/todo'
+import {addTodo, generateId} from './lib/todoHelpers'
+
+class App extends Component {
+   state = {
+      todos: [
+        {id: 1, name: 'test1', isComplete: true},
+        {id: 2, name: 'test2', isComplete: false},
+        {id: 3, name: 'test3', isComplete: false}
+      ],
+      currentTodo: ''
+    }
+
+  handleInputChange = (evt) => {
+    this.setState({
+      currentTodo: evt.target.value 
+    })
+  }
+
+  handleEmptySubmit = (evt) => {
+    evt.preventDefault()
+    this.setState({
+      errorMessage: 'please supply a todo name'
+    })
+  }
+
+  handleSubmit = (evt) => {
+      evt.preventDefault()
+      const newId = generateId()
+      const newTodo = {id: newId, name: this.state.currentTodo, isComplete: false}
+      const updatedTodos = addTodo(this.state.todos, newTodo)
+      this.setState({
+        todos: updatedTodos,
+        currentTodo: '',
+        errorMessage: ''
+      })
+
+    }
+
+  render() {
+    const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>React Todos</h2>
+        </div>
+        <div className="Todo-App">
+          {this.state.errorMessage && <span className='error'>{this.state.errorMessage}</span>}
+          <TodoForm handleInputChange={this.handleInputChange}
+            currentTodo={this.state.currentTodo}
+            handleSubmit={submitHandler}/>
+          <TodoList todos={this.state.todos}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/todo/TodoList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {TodoItem} from './TodoItem'
+
+export const TodoList = (props) => {
+  return (
+    <div className="Todo-list">
+      <ul>
+        {
+          props.todos.map(todo => 
+          <TodoItem key={todo.id} {...todo} />)
+        }
+      </ul>
+    </div>
+  )
+}
+
+TodoList.propTypes = {
+  todos: React.PropTypes.array.isRequired
+}
